d952573a2f7a8b8ff6830f66d301bdb42f5fe19b
szymonyang <chapter7even@gmail.com>
2020-06-26 14:59:58 +1000

    Dynamic showing item quanitity

--- store/views.py
+++ store/views.py
@@ -40,5 +40,16 @@ class CartView(View):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, is_complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        # Create empty cart for now for non-logged in user
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+        cartItems = order['get_cart_items']
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, "store/checkout.html", context)
