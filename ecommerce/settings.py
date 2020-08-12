d2940ef4ca5877522616b456da725613f5c73f67
szymonyang <chapter7even@gmail.com>
2020-08-12 23:20:09 +1000

    reorder     whitenoise.middleware.WhiteNoiseMiddleware,    django.contrib.sessions.middleware.SessionMiddleware,

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,11 +40,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     "django.middleware.security.SecurityMiddleware",
     # http://whitenoise.evans.io/en/stable/django.html
-
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
